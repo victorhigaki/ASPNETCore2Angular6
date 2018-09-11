da7fa0ed635f58c4f52f48a3ad56e5198d8e3886
Victor Higaki <higvic@gmail.com>
2018-09-11 00:21:53 -0300

    55

--- MyWebApp/Startup.cs
+++ MyWebApp/Startup.cs
@@ -20,15 +20,17 @@ namespace MyWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!"); // toda mensagem interceptada devolve hello world
+            //});
+
+            app.UseStaticFiles();
         }
     }
 }
