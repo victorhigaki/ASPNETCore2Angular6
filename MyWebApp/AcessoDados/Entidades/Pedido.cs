b61c0e81a299dfdc4a06a643b2c25ea915851985
Victor Higaki <higvic@gmail.com>
2018-09-12 23:15:59 -0300

    DB

---
+++ MyWebApp/AcessoDados/Entidades/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Entidades
+{
+    public class Pedido
+    {
+
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public ICollection<Pedido> Pedidos { get; set; }
+
+    }
+}
